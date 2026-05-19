c5e934d483cfe0ab36f45965c561664007b00902
Mathieu Roy <mathieu.givens@gmail.com>
2026-05-19 12:52:30 -0600

    Pay down council tool imports

--- src/tools/councils.rs
+++ src/tools/councils.rs
@@ -1,5 +1,11 @@
-use crate::server::*;
-use crate::*;
+use crate::council;
+use crate::server::BlackboxServer;
+use crate::tools::bro_params::{CouncilListParams, CouncilOpenParams, CouncilPostsParams};
+
+use rmcp::handler::server::router::tool::ToolRouter;
+use rmcp::handler::server::wrapper::Parameters;
+use rmcp::model::CallToolResult;
+use rmcp::{tool, tool_router};
 
 pub(crate) fn router() -> ToolRouter<BlackboxServer> {
     BlackboxServer::councils_tools()
